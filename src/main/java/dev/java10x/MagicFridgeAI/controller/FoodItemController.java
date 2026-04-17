1e35ce207d5effa8e60549bc0b4277f064424adc
TI-nando <fernandoagro.654@gmail.com>
2026-04-17 11:32:10 -0300

    feat: Criada as rotas no controller e os contrutores em service

--- src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
@@ -3,9 +3,8 @@ package dev.java10x.MagicFridgeAI.controller;
 import dev.java10x.MagicFridgeAI.model.FoodItem;
 import dev.java10x.MagicFridgeAI.service.FoodItemService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/food")
@@ -17,15 +16,40 @@ public class FoodItemController {
         this.service = service;
     }
 
+    // ----- Pontos de Acesso ou Endpoints -----
+
     //Post
+    @PostMapping
     public ResponseEntity<FoodItem> criar(@RequestBody FoodItem fooditem) {
         FoodItem salvo = service.salvar(fooditem);
         return ResponseEntity.ok(salvo);
     }
 
-    //Get
+    //Get - Listar todos
+    @GetMapping
+    public ResponseEntity<List<FoodItem>> listarTodos() {
+        List<FoodItem> listar = service.listar();
+        return ResponseEntity.ok(listar); // Retorna status 200 ok
+    }
+
+    //Get - Buscar por ID
+    @GetMapping("/{id}") // A URL será /food/1, /food/2, etc...
+    public ResponseEntity<FoodItem> buscar(@PathVariable Long id) { // @PathVariable pega o "{id}" da URL e coloca na variável "id"
+        FoodItem item = service.buscarPorId(id);
+        return ResponseEntity.ok(item);
+    }
 
     //Update
+    @PutMapping("/{id}")
+    public ResponseEntity<FoodItem> atualizar(@PathVariable Long id, @RequestBody FoodItem fooditem) {
+        FoodItem atualizado = service.atualizar(id, fooditem);
+        return ResponseEntity.ok(atualizado);
+    }
 
     //Delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build(); // Retorna 204 No Content
+    }
 }
--- src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
+++ src/main/java/dev/java10x/MagicFridgeAI/service/FoodItemService.java
@@ -23,4 +23,29 @@ public class FoodItemService {
     public List<FoodItem> listar() {
         return repository.findAll();
     }
+
+    public FoodItem buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Item não encontrado"));
+    }
+
+    public FoodItem atualizar(Long id, FoodItem dadosAtualizados) {
+        // 1. Busca o item antigo no banco
+        FoodItem itemAntigo = buscarPorId(id);
+
+        // 2. Atualizamos os atribustos (não atualiza o ID!)
+        itemAntigo.setName(dadosAtualizados.getName());
+        itemAntigo.setCategoria(dadosAtualizados.getCategoria());
+        itemAntigo.setQuantidade(dadosAtualizados.getQuantidade());
+        itemAntigo.setValidade(dadosAtualizados.getValidade());
+
+        // 3. Salvamos por cima
+        return repository.save(itemAntigo);
+    }
+
+    public void deletar(Long id) {
+        // Sera verificado se existe antes de tentar deletar
+        FoodItem item = buscarPorId(id);
+        repository.delete(item);
+    }
 }
